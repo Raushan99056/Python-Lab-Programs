9a0a28a3d97396153bbc35f9b905156d927cc81f
Raushan99056 <raushantiwary100@gmail.com>
2026-09-11 14:46:38 +0530

    Update program2_largest_of_three.py

--- program2_largest_of_three.py
+++ program2_largest_of_three.py
@@ -1,3 +1,5 @@
+Write a python program to find largest of three numbers.
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 num3 = float(input("Enter third number: "))
@@ -5,7 +7,7 @@ num3 = float(input("Enter third number: "))
 if num1 >= num2 and num1 >= num3:
 largest = num1
 elif num2 >= num1 and num2 >= num3:
-largest = num2
+largest = num2 
 else:
 largest = num3
 
