27d177177fd66f8ce26243658f62b7baf9bf18b9
Raushan99056 <raushantiwary100@gmail.com>
2026-09-11 14:40:03 +0530

    Create program10_numbers_10_to_99.py

---
+++ program10_numbers_10_to_99.py
@@ -0,0 +1,10 @@
+write a python program to print numbers between 10 to 99.
+
+
+# Program to print numbers between 10 and 99
+
+print("Numbers from 10 to 99")
+print("---------------------")
+
+for i in range(10, 100):
+    print(i)
