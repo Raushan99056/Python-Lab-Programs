a1ac00d6ed87e48bddc568fadf923872c62e758e
Raushan99056 <raushantiwary100@gmail.com>
2026-09-15 14:17:34 +0530

    Create program to print student details
    Added a program to print student details.

---
+++ program15_student_details.py
@@ -0,0 +1,17 @@
+write a python program to print following statements as an output using print statement. 
+Student Name:
+Contact_No:
+Mother Tongue:
+year:
+panel:
+Roll_No:
+
+
+
+
+print("Student Name:")
+print("Contact_No:")
+print("Mother Tongue:")
+print("Year:")
+print("Panel:")
+print("Roll_No:")
