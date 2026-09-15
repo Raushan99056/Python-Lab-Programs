91b3d35b4ea1dc0c83689dd75df54ddfb7b7c748
Raushan99056 <raushantiwary100@gmail.com>
2026-09-15 14:29:38 +0530

    Convert specific print statements to comments
    Modified print statements to multi-line comments to exclude certain fields from output.

---
+++ program15b_student_information.py
@@ -0,0 +1,13 @@
+In the previous code you written, modify the statements printing following fields into multi-line comments , so these fields will not be the part of the output.
+Address:
+Contact_No:
+Mother Tongue
+
+
+print("Student Name:")
+
+"""
+Address:
+Contact_No:
+Mother Tongue:
+"""
